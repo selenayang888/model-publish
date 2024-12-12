8da669d5eaea6261024a10b897dd44910883fce9
Selena Yang <syang0326@gmail.com>
2024-12-12 11:34:29 -0800

    install azure-cli

--- docker_script.sh
+++ docker_script.sh
@@ -5,6 +5,7 @@ pip install fastapi
 pip install uvicorn
 pip install onnxruntime-genai-cuda
 pip install pandas
+pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge
 pip install azure-ai-evaluation --upgrade
 pip install promptflow-azure --upgrade
 
