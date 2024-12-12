4298162fd0dfea6f95a7f837b595c7b4887604ad
Selena Yang <syang0326@gmail.com>
2024-12-12 12:59:42 -0800

    diagnosis

--- docker_script.sh
+++ docker_script.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+echo "Step.1 - installing fastapi uvicorn onnxruntime-genai-cuda pandas"
+pip install fastapi uvicorn onnxruntime-genai-cuda pandas
 
-pip install fastapi
-pip install uvicorn
-pip install onnxruntime-genai-cuda
-pip install pandas
+echo "Step.2 - azure-cli"
 pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge
+
+echo "Step.3 - azure-ai"
 pip install azure-ai-evaluation --upgrade
+
+echo "Step.4 - promptflow-azure"
 pip install promptflow-azure --upgrade
 
+echo "Diagosis:"
+
+conda env list
+
+echo "pip list:"
+pip list
 
 nvcc --version
 nvidia-smi
