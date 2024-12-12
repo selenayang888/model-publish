413fbb39beeadc152e94b181fe9957e9503b87df
Selena Yang <syang0326@gmail.com>
2024-12-12 00:14:44 -0800

    install azure-ai-eval

--- docker_script.sh
+++ docker_script.sh
@@ -5,6 +5,8 @@ pip install fastapi
 pip install uvicorn
 pip install onnxruntime-genai-cuda
 pip install pandas
+pip install azure-ai-evaluation --upgrade
+pip install promptflow-azure --upgrade
 
 cd /ort_src
 python ./docker_main.py
