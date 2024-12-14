7d0a6d46dff60a729359f6ea88bade466c86a5a3
Selena Yang <syang0326@gmail.com>
2024-12-13 16:10:19 -0800

    update python to 3.12 version

--- docker_script.sh
+++ docker_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+conda install python=3.12
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
 dpkg -i cuda-keyring_1.1-1_all.deb
 
