90aaeb6d6348d0c9e54ac84f79cd172073657100
Selena Yang <syang0326@gmail.com>
2025-01-11 00:31:32 -0800

    continue fixing CUDA error

--- docker_export_model_script.sh
+++ docker_export_model_script.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+
+git clone https://github.com/apsonawane/turnkeyml-cuda.git
+cd turnkeyml-cuda
+git reset --hard f15fac9638fc03a3a83fff1d14d00e2246edeb63
+conda create -n tk-llm python=3.12
+source /opt/conda/etc/profile.d/conda.sh
+echo "Activating conda environment"
+conda activate tk-llm
+echo "Installing turnkeyllm"
+pip install -e .[llm-oga-cuda]
+echo "Installed turnkeyllm"
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
 dpkg -i cuda-keyring_1.1-1_all.deb
 
 apt-get update
-apt-get install -y
+
 apt-get -y install cudnn9-cuda-12
-
-
-# echo "Step.1 - onnxruntime-genai-cuda"
-# pip install onnxruntime-genai-cuda
 
 
 pip install -y huggingface_hub
@@ -24,17 +32,6 @@ nvidia-smi
 
 echo "find libcudnn"
 find / -name libcudnn.so.*
-
-git clone https://github.com/apsonawane/turnkeyml-cuda.git
-cd turnkeyml-cuda
-git reset --hard f15fac9638fc03a3a83fff1d14d00e2246edeb63
-conda create -n tk-llm python=3.10
-source /opt/conda/etc/profile.d/conda.sh
-echo "Activating conda environment"
-conda activate tk-llm
-echo "Installing turnkeyllm"
-pip install -e .[llm-oga-cuda]
-echo "Installed turnkeyllm"
 
 echo "Download baseline model"
 huggingface-cli download $MODEL_NAME --local-dir /build/oga_models/hf_version/
