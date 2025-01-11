fe59e01495aa51410b40ad0a6d2ba7ee1fd505b8
Selena Yang <syang0326@gmail.com>
2025-01-10 17:22:09 -0800

    update git branch to rui-ren

--- docker_export_model_script.sh
+++ docker_export_model_script.sh
@@ -27,7 +27,7 @@ nvidia-smi
 echo "find libcudnn"
 find / -name libcudnn.so.*
 
-git clone https://github.com/apsonawane/turnkeyml-cuda.git
+git clone https://github.com/rui-ren/turnkeyml-cuda.git
 cd turnkeyml-cuda
 conda create -n tk-llm python=3.10
 source /opt/conda/etc/profile.d/conda.sh
