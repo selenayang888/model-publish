f7a513803a4aadcae0ad02badf5983ffb4425575
Selena Yang <syang0326@gmail.com>
2025-01-10 23:00:51 -0800

    not checking out

--- docker_export_model_script.sh
+++ docker_export_model_script.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 
-git clone https://github.com/apsonawane/turnkeyml-cuda.git
-cd turnkeyml-cuda
-git reset --hard f15fac9638fc03a3a83fff1d14d00e2246edeb63
+# git clone https://github.com/apsonawane/turnkeyml-cuda.git
+# cd turnkeyml-cuda
+# git reset --hard f15fac9638fc03a3a83fff1d14d00e2246edeb63
 conda create -n tk-llm python=3.12
-source /opt/conda/etc/profile.d/conda.sh
+# source /opt/conda/etc/profile.d/conda.sh
 echo "Activating conda environment"
 conda activate tk-llm
 echo "Installing turnkeyllm"
-pip install -e .[llm-oga-cuda]
+# pip install -e .[llm-oga-cuda]
 echo "Installed turnkeyllm"
 
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
 dpkg -i cuda-keyring_1.1-1_all.deb
 
 apt-get update
-apt-get install -y
 apt-get -y install cudnn9-cuda-12
 
 
