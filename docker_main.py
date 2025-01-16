a8b19684e3f168856e7570d8a8d63b923b704639
Selena Yang <syang0326@gmail.com>
2025-01-15 18:12:46 -0800

    update the pipeline

--- docker_main.py
+++ docker_main.py
@@ -6,6 +6,7 @@ from evaluate_models_target_eci import async_main_eci
 from evaluate_models_target_jailbreak import async_main_jailbreak
 from evaluate_models_target_ground import async_main_ground
 from evaluate_models_target_safety import async_main_safety
+from evaluate_models_target_summarization import async_main_summarization
 
 import os
 
@@ -24,6 +25,7 @@ print(" ### Start model endpoint :)")
 import asyncio
 
 loop = asyncio.get_event_loop()
+
 
 # Test the IP
 try:
@@ -52,6 +54,12 @@ except Exception as e:
 # Test the Harmful Content
 try:
     loop.run_until_complete(async_main_safety())
+except Exception as e:
+    pass
+
+# Test the Harmful Summarization
+try:
+    loop.run_until_complete(async_main_summarization())
 except Exception as e:
     pass
 
--- docker_main_baseline.py
+++ docker_main_baseline.py
@@ -6,8 +6,10 @@ from evaluate_models_target_eci import async_main_eci
 from evaluate_models_target_jailbreak import async_main_jailbreak
 from evaluate_models_target_ground import async_main_ground
 from evaluate_models_target_safety import async_main_safety
+from evaluate_models_target_summarization import async_main_summarization
 
 import os
+
 print("### ENV in docker_main_baseline.py")
 print(os.environ)
 
@@ -22,15 +24,16 @@ time.sleep(70)
 
 
 import asyncio
-loop = asyncio.get_event_loop()
-# loop.run_until_complete(async_main(baseline_only=True))
-# loop.run_until_complete(async_main_eci(baseline_only=True))
-# loop.run_until_complete(async_main_jailbreak(baseline_only=True))
-loop.run_until_complete(async_main_ground(baseline_only=True))
-# loop.run_until_complete(async_main_safety(baseline_only=True))
 
+loop = asyncio.get_event_loop()
+
+loop.run_until_complete(async_main(baseline_only=True))
+loop.run_until_complete(async_main_eci(baseline_only=True))
+loop.run_until_complete(async_main_jailbreak(baseline_only=True))
+loop.run_until_complete(async_main_ground(baseline_only=True))
+loop.run_until_complete(async_main_safety(baseline_only=True))
+loop.run_until_complete(async_main_summarization(baseline_only=True))
 
 
 uvicorn_proc.send_signal(subprocess.signal.SIGTERM)
 uvicorn_proc.wait()
-
