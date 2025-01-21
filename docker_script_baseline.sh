2ac91be4becbade196fd95430f038a1b823c9bc9
Rui-Ren <ruiren1225@gmail.com>
2025-01-21 14:41:55 -0800

    fix flash attn

--- docker_script_baseline.sh
+++ docker_script_baseline.sh
@@ -13,8 +13,8 @@ echo "Step.1 - installing fastapi uvicorn onnxruntime-genai-cuda"
 pip install fastapi uvicorn onnxruntime-genai-cuda
 
 echo "step.1b - installing baselina model requirement"
-pip install torch==2.3.1
-pip install flash_attn accelerate==0.31.0 transformers
+pip install torch==2.5.1
+pip install flash_attn==2.7.3 accelerate==0.31.0 transformers==4.48.1
 
 echo "Step.2 - azure-cli"
 pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge
