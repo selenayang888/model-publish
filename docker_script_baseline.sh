7d4ca99972c78bc1300a7244a9f40155ca54af16
Rui-Ren <ruiren1225@gmail.com>
2025-01-21 15:44:38 -0800

    baseline model

--- docker_script_baseline.sh
+++ docker_script_baseline.sh
@@ -3,22 +3,12 @@
 conda install python=3.12
 
 
-echo "Step.1 - installing fastapi uvicorn onnxruntime-genai-cuda"
-pip install fastapi uvicorn onnxruntime-genai-cuda
 
-echo "step.1b - installing baselina model requirement"
-pip install torch==2.5.1
-
-# ACPT will cover it.
-
-pip install flash_attn==2.7.3 accelerate==0.31.0 transformers==4.48.1
+echo "step.1 - installing baseline model requirement"
+pip install -r ./requirements/requirements_baseline.txt
 
 echo "Step.2 - azure-cli"
 pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge
-
-echo "Step.3 - azure-ai and promptflow"
-pip install azure-ai-evaluation --upgrade promptflow-azure marshmallow==3.23.2
-
 
 az login --identity
 echo "print out az account"
