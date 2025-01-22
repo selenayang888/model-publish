82b8a81e02dca7680376e1635ba8d7adf3d55e7a
Rui-Ren <ruiren1225@gmail.com>
2025-01-22 08:34:47 -0800

    update wordir

--- docker_script_baseline.sh
+++ docker_script_baseline.sh
@@ -4,8 +4,11 @@ conda install python=3.12
 
 echo "y" | pip uninstall torch
 
+# Get into working directory
+cd /ort_src
+
 echo "step.1 - installing baseline model requirement"
-pip install -r ./requirements_baseline.txt
+pip install -r ./requirements/requirements_baseline.txt
 
 echo "Step.2 - azure-cli"
 pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge
@@ -14,5 +17,4 @@ az login --identity
 echo "print out az account"
 az account show
 
-cd /ort_src
 python ./docker_main_baseline.py
