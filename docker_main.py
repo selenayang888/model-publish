ed53ddab6d77230903a013471808eb3fdbc25191
Rui-Ren <ruiren1225@gmail.com>
2025-01-23 11:55:49 -0800

    pass exception

--- docker_main.py
+++ docker_main.py
@@ -8,6 +8,7 @@ from evaluate_models_target_ground import async_main_ground
 from evaluate_models_target_safety import async_main_safety
 
 import os
+
 print("### ENV in docker_main.py")
 print(os.environ)
 
@@ -21,17 +22,39 @@ time.sleep(20)
 print(" ### Start model endpoint :)")
 
 import asyncio
+
 loop = asyncio.get_event_loop()
-# loop.run_until_complete(async_main())
-# loop.run_until_complete(async_main_eci())
-# loop.run_until_complete(async_main_jailbreak())
-loop.run_until_complete(async_main_ground())
-# loop.run_until_complete(async_main_safety())
 
+# Test the IP
+try:
+    loop.run_until_complete(async_main())
+except Exception as e:
+    pass
 
+# Test the eci
+try:
+    loop.run_until_complete(async_main_eci())
+except Exception as e:
+    pass
+
+# Test the jailbreaking
+try:
+    loop.run_until_complete(async_main_jailbreak())
+except Exception as e:
+    pass
+
+# Test the groundness
+try:
+    loop.run_until_complete(async_main_ground())
+except Exception as e:
+    pass
+
+# Test the Harmful Content
+try:
+    loop.run_until_complete(async_main_safety())
+except Exception as e:
+    pass
 
 # step.last - send SIGTERM to server and wait for it to exit
 uvicorn_proc.send_signal(subprocess.signal.SIGTERM)
 uvicorn_proc.wait()
-
-
