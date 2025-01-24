6c8b05191a2a788bcdc16e757d20e213da98c361
Rui-Ren <ruiren1225@gmail.com>
2025-01-23 16:19:13 -0800

    lint

--- main.py
+++ main.py
@@ -7,27 +7,25 @@ import argparse
 app = FastAPI()
 
 import os
+
 print("### ENV in main.py (server)")
 print(os.environ)
 
 
-# Initialize the model, tokenizer, and tokenizer stream
-# Please update the DML model here
-#model = og.Model('/model')
-model = og.Model('/model')
+model = og.Model("/model")
 tokenizer = og.Tokenizer(model)
 tokenizer_stream = tokenizer.create_stream()
 
 print("The model is loaded!!")
 
 # Set the search options
-search_options = {
-    'max_length': 2048
-}
+search_options = {"max_length": 2048}
+
 
 # Define the input schema
 class InputData(BaseModel):
     text: str
+
 
 @app.post("/score")
 async def score(input_data: InputData):
@@ -36,7 +34,7 @@ async def score(input_data: InputData):
     print("### main.py: start run")
     # The chat template needs to update later.
     # I will give you the OpenAIChatCompletion template.
-    chat_template = '<|user|>\n{input} <|end|>\n<|assistant|>'
+    chat_template = "<|user|>\n{input} <|end|>\n<|assistant|>"
     prompt = chat_template.format(input=text)
 
     input_tokens = tokenizer.encode(prompt)
@@ -49,17 +47,14 @@ async def score(input_data: InputData):
     try:
         generator.append_tokens(input_tokens)
         while not generator.is_done():
-            
+
             generator.generate_next_token()
             new_token = generator.get_next_tokens()[0]
-            
+
             output_text += tokenizer_stream.decode(new_token)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error during generation: {e}")
     finally:
         del generator
 
-
     return {"response": output_text}
-
-#uvicorn main:app --reload 
--- main_pytorch.py
+++ main_pytorch.py
@@ -9,19 +9,14 @@ app = FastAPI()
 
 torch.random.manual_seed(0)
 
-# Initialize the model, tokenizer, and tokenizer stream
-# Please update the DML model here
-# model = og.Model('/home/RAI-SDK/hf_version')
-# tokenizer = og.Tokenizer(model)
 model = AutoModelForCausalLM.from_pretrained(
-    "/baseline_model", 
-    device_map="cuda", 
-    torch_dtype="auto", 
-    trust_remote_code=True, 
+    "/baseline_model",
+    device_map="cuda",
+    torch_dtype="auto",
+    trust_remote_code=True,
 )
 
 tokenizer = AutoTokenizer.from_pretrained("/baseline_model")
-#tokenizer_stream = tokenizer.create_stream()
 
 pipe = pipeline(
     "text-generation",
@@ -32,64 +27,38 @@ pipe = pipeline(
 print("The baseline model is loaded!!")
 
 # Set the search options
-search_options = {
-    'max_length': 4096
-}
+search_options = {"max_length": 4096}
+
 
 # Define the input schema
 class InputData(BaseModel):
     text: str
 
+
 @app.post("/score")
 async def score(input_data: InputData):
     text = input_data.text
-    
+
     print("### main_baseline.py: start run")
     # The chat template needs to update later.
     # I will give you the OpenAIChatCompletion template.
-    chat_template = '<|user|>\n{input} <|end|>\n<|assistant|>'
+    chat_template = "<|user|>\n{input} <|end|>\n<|assistant|>"
     prompt = chat_template.format(input=text)
 
-    #input_tokens = tokenizer.encode(prompt)
-
-    # params = og.GeneratorParams(model)
-    # params.set_search_options(**search_options)
-    # params.input_ids = input_tokens
-    # generator = og.Generator(model, params)
-
-    # output_text = ""
-    # try:
-    #     while not generator.is_done():
-    #         generator.compute_logits()
-    #         generator.generate_next_token()
-
-    #         new_token = generator.get_next_tokens()[0]
-    #         output_text += tokenizer_stream.decode(new_token)
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=f"Error during generation: {e}")
-    # finally:
-    #     del generator
-
-   
-
     messages = [
-        #{"role": "system", "content": "You are a helpful AI assistant."},
         {"role": "user", "content": text},
-        #{"role": "assistant", "content": "Sure! Here are some ways to eat bananas and dragonfruits together: 1. Banana and dragonfruit smoothie: Blend bananas and dragonfruits together with some milk and honey. 2. Banana and dragonfruit salad: Mix sliced bananas and dragonfruits together with some lemon juice and honey."},
-        #{"role": "user", "content": "What about solving an 2x + 3 = 7 equation?"},
     ]
 
-
-
     generation_args = {
-        "max_new_tokens": 2048, 
+        "max_new_tokens": 2048,
         "return_full_text": False,
         "temperature": 0.0,
         "do_sample": False,
     }
 
-    output_text = pipe(messages, **generation_args)[0]['generated_text']
+    output_text = pipe(messages, **generation_args)[0]["generated_text"]
 
     return {"response": output_text}
 
-#uvicorn main:app --reload 
+
+# uvicorn main:app --reload
