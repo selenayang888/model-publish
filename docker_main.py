c90ebe3e0e55026853ff4b9875fb2eaf01cbd937
Rui-Ren <ruiren1225@gmail.com>
2025-01-23 16:23:30 -0800

    eci

--- docker_main.py
+++ docker_main.py
@@ -28,16 +28,16 @@ loop = asyncio.get_event_loop()
 
 
 # Test the IP
-try:
-    loop.run_until_complete(async_main())
-except Exception as e:
-    pass
-
-# # Test the eci
 # try:
-#     loop.run_until_complete(async_main_eci())
+#     loop.run_until_complete(async_main())
 # except Exception as e:
 #     pass
+
+# Test the eci
+try:
+    loop.run_until_complete(async_main_eci())
+except Exception as e:
+    pass
 
 # # Test the jailbreaking
 # try:
