cce643c4a8f6dbc7f426d672c7a52a1681f50d1f
Rui-Ren <ruiren1225@gmail.com>
2025-01-23 17:04:17 -0800

    update the download path

--- docker_export_model_script.sh
+++ docker_export_model_script.sh
@@ -40,58 +40,16 @@ echo "Download baseline model"
 
 
 echo "Running lemonade command"
-# lemonade -i $MODEL_NAME --cache-dir "/build" oga-load --input_path "/build/phi-4-mini-instruct-01072025/hf_version" --device cuda --dtype int4 accuracy-mmlu --tests management
+lemonade -i $MODEL_NAME --cache-dir "/build" oga-load --input_path "/build/phi-4-mini-instruct-01222025/hf_version" --device cuda --dtype int4 accuracy-mmlu --tests management
 
-lemonade -i $MODEL_NAME --cache-dir "/build" oga-load --device cuda --dtype int4 accuracy-mmlu --tests management
-
-
-# echo "Copying the model to ort_src"
 
 ls -la "/build/oga_models/"
 
 mkdir /build/oga_models/baseline_model/
 
-cp -r /build/phi-4-mini-instruct-01072025/hf_version/* /build/oga_models/baseline_model/
+cp -r /build/phi-4-mini-instruct-01222025/hf_version/* /build/oga_models/baseline_model/
 
 ls -la "/build/oga_models/"
 
 echo "lemonade exported onnx model successfully!"
 
-# mkdir /model
-
-# cp -r "/root/oga_models/microsoft_phi-3-mini-4k-instruct/" "/model"
-
-# ls "/model"
-
-# conda create -n rai python==3.12
-# conda activate rai
-
-# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
-# dpkg -i cuda-keyring_1.1-1_all.deb
-
-# apt-get update
-# apt-get -y install cudnn9-cuda-12
-
-
-# echo "Step.1 - installing fastapi uvicorn onnxruntime-genai-cuda"
-# pip install fastapi uvicorn onnxruntime-genai-cuda
-
-# echo "step.1b - installing baselina model requirement"
-# pip install torch==2.3.1
-# pip install flash_attn accelerate==0.31.0 transformers==4.43.0
-
-# echo "Step.2 - azure-cli"
-# pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge
-
-# echo "Step.3 - azure-ai"
-# pip install azure-ai-evaluation --upgrade
-
-# echo "Step.4 - promptflow-azure"
-# pip install promptflow-azure --upgrade
-
-# az login --identity
-# echo "print out az account"
-# az account show
-
-# cd /ort_src
-# python ./docker_main.py
--- docker_main.py
+++ docker_main.py
@@ -28,36 +28,36 @@ loop = asyncio.get_event_loop()
 
 
 # Test the IP
-# try:
-#     loop.run_until_complete(async_main())
-# except Exception as e:
-#     pass
+try:
+    loop.run_until_complete(async_main())
+except Exception as e:
+    pass
 
 # Test the eci
-# try:
-#     loop.run_until_complete(async_main_eci())
-# except Exception as e:
-#     pass
+try:
+    loop.run_until_complete(async_main_eci())
+except Exception as e:
+    pass
 
-# # Test the jailbreaking
-# try:
-#     loop.run_until_complete(async_main_jailbreak())
-# except Exception as e:
-#     pass
+# Test the jailbreaking
+try:
+    loop.run_until_complete(async_main_jailbreak())
+except Exception as e:
+    pass
 
-# # Test the groundness
-# try:
-#     loop.run_until_complete(async_main_ground())
-# except Exception as e:
-#     pass
+# Test the groundness
+try:
+    loop.run_until_complete(async_main_ground())
+except Exception as e:
+    pass
 
-# # Test the Harmful Content
-# try:
-#     loop.run_until_complete(async_main_safety())
-# except Exception as e:
-#     pass
+# Test the Harmful Content
+try:
+    loop.run_until_complete(async_main_safety())
+except Exception as e:
+    pass
 
-# # Test the Harmful Summarization
+# Test the Harmful Summarization
 try:
     loop.run_until_complete(async_main_summarization())
 except Exception as e:
