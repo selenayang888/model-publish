55b5527fb332a3de07106049f03b839729cbaf48
Rui-Ren <ruiren1225@gmail.com>
2025-01-24 18:18:24 -0800

    groundness

--- docker_main.py
+++ docker_main.py
@@ -41,14 +41,14 @@ loop = asyncio.get_event_loop()
 
 # # Test the jailbreaking
 
-loop.run_until_complete(async_main_jailbreak())
+# loop.run_until_complete(async_main_jailbreak())
 
 
 # Test the groundness
-# try:
-#     loop.run_until_complete(async_main_ground())
-# except Exception as e:
-#     pass
+try:
+    loop.run_until_complete(async_main_ground())
+except Exception as e:
+    pass
 
 # Test the Harmful Content
 # try:
