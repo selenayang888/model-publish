bcce9a5bb056c3bb3c9d3ecf821f82e6bc940803
Rui-Ren <ruiren1225@gmail.com>
2025-01-24 18:38:27 -0800

    fp16

--- docker_export_model_script.sh
+++ docker_export_model_script.sh
@@ -40,7 +40,7 @@ echo "Download baseline model"
 
 
 echo "Running lemonade command"
-lemonade -i $MODEL_NAME --cache-dir "/build" oga-load --input_path "/build/phi-4-mini-instruct-01222025/hf_version" --device cuda --dtype int4 accuracy-mmlu --tests management
+lemonade -i $MODEL_NAME --cache-dir "/build" oga-load --input_path "/build/phi-4-mini-instruct-01222025/hf_version" --device cuda --dtype fp16 accuracy-mmlu --tests management
 
 
 ls -la "/build/oga_models/"
