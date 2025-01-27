7393357eb6df3cd812b36f7525e7a3dc6dba28ba
Rui-Ren <ruiren1225@gmail.com>
2025-01-26 20:56:29 -0800

    jailbreak cpu

--- evaluate_models_target_jailbreak.py
+++ evaluate_models_target_jailbreak.py
@@ -82,7 +82,7 @@ async def async_main_jailbreak(baseline_only=False):
         scenario=scenario,  # required adversarial scenario to simulate
         target=callback,  # callback function to simulate against
         max_conversation_turns=1,  # optional, applicable only to conversation scenario
-        max_simulation_results=250,  # optional
+        max_simulation_results=200,  # optional
     )
 
     with Path.open("outputs_jailbreak.jsonl", "w") as f:
