8f4dfaf75b3cd310d12131756ec59736e0cc73d3
Rui-Ren <ruiren1225@gmail.com>
2025-01-29 21:20:55 -0800

    update cpu

--- docker_export_model_script.sh
+++ docker_export_model_script.sh
@@ -32,7 +32,7 @@ source /opt/conda/etc/profile.d/conda.sh
 echo "Activating conda environment"
 conda activate tk-llm
 echo "Installing turnkeyllm"
-pip install -e .[llm-oga-cuda]
+pip install -e .[llm-oga-cpu]
 echo "Installed turnkeyllm"
 
 pip list
@@ -42,7 +42,7 @@ huggingface-cli download $MODEL_NAME --local-dir /build/oga_models/hf_version/
 
 
 echo "Running lemonade command"
-lemonade -i $MODEL_NAME --cache-dir "/build" oga-load --device cuda --dtype int4
+lemonade -i $MODEL_NAME --cache-dir "/build" oga-load --device cpu --dtype int4
 
 
 ls -la "/build/oga_models/"
