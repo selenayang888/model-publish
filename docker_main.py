37f9669d7e009f4170ebec9c12ca5d1731dd4038
Rui-Ren <ruiren1225@gmail.com>
2025-01-30 16:54:02 -0800

    groundness,harmfulcontent,summarization

--- docker_main.py
+++ docker_main.py
@@ -41,28 +41,28 @@ loop = asyncio.get_event_loop()
 
 # # Test the jailbreaking
 
+# try:
+#     loop.run_until_complete(async_main_jailbreak())
+# except Exception as e:
+#     pass
+
+# # # Test the groundness
 try:
-    loop.run_until_complete(async_main_jailbreak())
+    loop.run_until_complete(async_main_ground())
 except Exception as e:
     pass
 
-# # # Test the groundness
-# try:
-#     loop.run_until_complete(async_main_ground())
-# except Exception as e:
-#     pass
+# # Test the Harmful Content
+try:
+    loop.run_until_complete(async_main_safety())
+except Exception as e:
+    pass
 
-# # # Test the Harmful Content
-# try:
-#     loop.run_until_complete(async_main_safety())
-# except Exception as e:
-#     pass
-
-# # # Test the Harmful Summarization
-# try:
-#     loop.run_until_complete(async_main_summarization())
-# except Exception as e:
-#     pass
+# # Test the Harmful Summarization
+try:
+    loop.run_until_complete(async_main_summarization())
+except Exception as e:
+    pass
 
 # step.last - send SIGTERM to server and wait for it to exit
 uvicorn_proc.send_signal(subprocess.signal.SIGTERM)
