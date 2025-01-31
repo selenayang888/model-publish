6a1b933b1aa6a2ab1c731aba60ca93f43673694d
Rui-Ren <ruiren1225@gmail.com>
2025-01-31 15:12:33 -0800

    only test groundness

--- docker_main.py
+++ docker_main.py
@@ -28,23 +28,23 @@ loop = asyncio.get_event_loop()
 
 
 # # Test the IP
-try:
-    loop.run_until_complete(async_main())
-except Exception as e:
-    pass
+# try:
+#     loop.run_until_complete(async_main())
+# except Exception as e:
+#     pass
 
 # # Test the eci
-try:
-    loop.run_until_complete(async_main_eci())
-except Exception as e:
-    pass
+# try:
+#     loop.run_until_complete(async_main_eci())
+# except Exception as e:
+#     pass
 
 # # Test the jailbreaking
 
-try:
-    loop.run_until_complete(async_main_jailbreak())
-except Exception as e:
-    pass
+# try:
+#     loop.run_until_complete(async_main_jailbreak())
+# except Exception as e:
+#     pass
 
 # # Test the groundness
 try:
@@ -53,16 +53,16 @@ except Exception as e:
     pass
 
 # # Test the Harmful Content
-try:
-    loop.run_until_complete(async_main_safety())
-except Exception as e:
-    pass
+# try:
+#     loop.run_until_complete(async_main_safety())
+# except Exception as e:
+#     pass
 
 # # Test the Harmful Summarization
-try:
-    loop.run_until_complete(async_main_summarization())
-except Exception as e:
-    pass
+# try:
+#     loop.run_until_complete(async_main_summarization())
+# except Exception as e:
+#     pass
 
 # step.last - send SIGTERM to server and wait for it to exit
 uvicorn_proc.send_signal(subprocess.signal.SIGTERM)
--- docker_main_baseline.py
+++ docker_main_baseline.py
@@ -27,12 +27,12 @@ import asyncio
 
 loop = asyncio.get_event_loop()
 
-loop.run_until_complete(async_main(baseline_only=True))
-loop.run_until_complete(async_main_eci(baseline_only=True))
-loop.run_until_complete(async_main_jailbreak(baseline_only=True))
+# loop.run_until_complete(async_main(baseline_only=True))
+# loop.run_until_complete(async_main_eci(baseline_only=True))
+# loop.run_until_complete(async_main_jailbreak(baseline_only=True))
 loop.run_until_complete(async_main_ground(baseline_only=True))
-loop.run_until_complete(async_main_safety(baseline_only=True))
-loop.run_until_complete(async_main_summarization(baseline_only=True))
+# loop.run_until_complete(async_main_safety(baseline_only=True))
+# loop.run_until_complete(async_main_summarization(baseline_only=True))
 
 
 uvicorn_proc.send_signal(subprocess.signal.SIGTERM)
