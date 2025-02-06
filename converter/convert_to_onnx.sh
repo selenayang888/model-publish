e9fa07201dbf4ef316666cc8cc26ea6b0d2d8015
Rui-Ren <ruiren1225@gmail.com>
2025-02-05 20:25:41 -0800

    add cpu test

--- converter/convert_to_onnx.sh
+++ converter/convert_to_onnx.sh
@@ -4,27 +4,36 @@
 source ../turnkeyml.sh
 
 # TODO: rui-ren adapt for cpu version
-pip install -e .[llm-oga-gpu]
+
 echo "Installed turnkeyllm"
+
+if [[ $DEVICE_TYPE == "cpu" ]]; then
+    echo "Running on CPU"
+    pip install -e .[llm-oga-cpu]
+
+else
+    echo "Running on GPU"
+    pip install -e .[llm-oga-gpu]
+
+fi
 
 echo "Download baseline model"
 # huggingface-cli download $MODEL_NAME --local-dir /build/oga_models/hf_version/
 
-# TODO: rui-ren  we need to add a condition check here 
+# TODO: rui-ren  we need to add a condition check here
+# We need to handle 100 models
 huggingface-cli download $MODEL_NAME --local-dir /build/oga_models/hf_version/
 
 echo "Running lemonade command"
 # lemonade -i $MODEL_NAME --cache-dir "/build" oga-load --input_path "/build/phi-4-mini-instruct-01072025/hf_version" --device cuda --dtype int4 accuracy-mmlu --tests management
 
-lemonade -i $MODEL_NAME --cache-dir "/build" oga-load --device cpu --dtype int4
-
+lemonade -i $MODEL_NAME --cache-dir "/build" oga-load --device $DEVICE_TYPE --dtype $DATA_TYPE
 
 # echo "Copying the model to ort_src"
 
 ls -la "/build/oga_models/"
 
 mkdir /build/oga_models/baseline_model/
-
 
 ls -la "/build/oga_models/"
 
